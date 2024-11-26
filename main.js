5a9fb7eee974081402235ad76bcb51c6f2bb0c3e
Suhyun <suhyun_4951@naver.com>
2024-11-26 16:13:09 +0900

    game-3 making bullet

--- main.js
+++ main.js
@@ -14,6 +14,22 @@ let backgroundImage, spaceshipImage, enemyImage, bulletImage, gameoverImage;
 let spaceshipX = canvas.width/2 - 32
 let spaceshipY = canvas.height - 64
 
+let bulletList = [] // 총알 저장 리스트
+
+function Bullet() {
+    this.x = 0
+    this.y = 0
+    this.init = function() {
+        this.x = spaceshipX + 20
+        this.y = spaceshipY
+
+        bulletList.push(this)
+    }
+    this.update = function() {
+        this.y -= 7
+    }
+}
+
 function loadImage() {
     backgroundImage = new Image();
     backgroundImage.src="images/background.jpg";
@@ -35,12 +51,18 @@ let keysDown={}
 function setupKeyboardListeners() {
     document.addEventListener("keydown", function(event){
         keysDown[event.keyCode] = true;
-        console.log("keydown", event.keyCode)
     })
     document.addEventListener("keyup", function(){
         delete keysDown[event.keyCode]
-        console.log("keyup", event.keyCode)
+        if(event.keyCode == 32){
+            createBullet() // making bullet function
+        }
     })
+}
+
+function createBullet() {
+    let b = new Bullet()
+    b.init()
 }
 
 function update() {
@@ -56,12 +78,22 @@ function update() {
     if(spaceshipX >= canvas.width - 64){
         spaceshipX = canvas.width - 64
     }
+
+    // 총알 y 좌표 업데이트 하는 함수 호출
+    for (let i = 0; i < bulletList.length; i++){
+        bulletList[i].update()
+    }
 }
 
 function render() {
     // drawImage(image, x, y, width, height)
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+
+    // 총알 그려주기
+    for (let i = 0; i < bulletList.length; i++){
+        ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y)
+}
 }
 
 function main() {
